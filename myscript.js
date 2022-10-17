f2de669110ea98313b37a70edde7c3f7b76e9408
B114A <111514882+B114A@users.noreply.github.com>
2022-10-17 20:18:17 +1100

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,30 @@
+var vg_1 = "Apple_Prod.vg.json";
+vegaEmbed("#map1", vg_1).then(function(result) {
+
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_1 = "Banana_Prod.vg.json";
+vegaEmbed("#map2", vg_1).then(function(result) {
+
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_1 = "Orange_Prod.vg.json";
+vegaEmbed("#map3", vg_1).then(function(result) {
+
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_1 = "StackedBar_AreaHarvested.vg.json";
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_1 = "LineChart_TotalProd.vg.json";
+vegaEmbed("#line_chart", vg_1).then(function(result) {
+
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_1 = "TripleChartTop1.json";
+vegaEmbed("#triple_chart", vg_1).then(function(result) {
+
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
